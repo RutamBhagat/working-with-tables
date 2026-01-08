28a622f56210e4fabde9274984e76c19cf283aaf
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-08 14:14:13 +0530

    feat: add initial migration

---
+++ packages/db/src/migrations/20260108084210_mysterious_tusk/migration.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "comment" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "comment_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"text" text NOT NULL,
+	"user_id" integer,
+	"photo_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE "like" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "like_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer,
+	"photo_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE "photo" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "photo_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"url" text NOT NULL,
+	"user_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "user_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"username" text NOT NULL UNIQUE,
+	"email" text NOT NULL UNIQUE
+);
+--> statement-breakpoint
+ALTER TABLE "comment" ADD CONSTRAINT "comment_user_id_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id");--> statement-breakpoint
+ALTER TABLE "comment" ADD CONSTRAINT "comment_photo_id_photo_id_fkey" FOREIGN KEY ("photo_id") REFERENCES "photo"("id");--> statement-breakpoint
+ALTER TABLE "like" ADD CONSTRAINT "like_user_id_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id");--> statement-breakpoint
+ALTER TABLE "like" ADD CONSTRAINT "like_photo_id_photo_id_fkey" FOREIGN KEY ("photo_id") REFERENCES "photo"("id");--> statement-breakpoint
+ALTER TABLE "photo" ADD CONSTRAINT "photo_user_id_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id");
